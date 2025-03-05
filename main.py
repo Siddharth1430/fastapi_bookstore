b37b6cbe7b655d2c3090b535dd2b70fe25308850
Kanishkaprabhas <kanishkabalaa355@gmail.com>
2025-03-05 10:50:47 +0530

    Added Doc Tag and Modified gitignore

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from auth import get_current_user
 security = HTTPBearer()
 app=FastAPI()
 
-@app.post("/generateotp")
+@app.post("/generateotp",tags =["Login"])
 def generate_otp(payload:OTPGenerator,db : Session= Depends(get_db)):
     """_summary_
     Generates an OTP for phone number authentication.
@@ -32,7 +32,7 @@ def generate_otp(payload:OTPGenerator,db : Session= Depends(get_db)):
     results = OTPAuthentication(db)
     return results.generate_otp(payload)
 
-@app.post("/verify")
+@app.post("/verify",tags =["Login"])
 def verify_via_phone_number(payload:Verifier,db: Session= Depends(get_db)):
     """_summary_
      Verifies the OTP sent to the user's phone number.
@@ -45,7 +45,7 @@ def verify_via_phone_number(payload:Verifier,db: Session= Depends(get_db)):
     results = OTPAuthentication(db)
     return results.verify_via_phone_number(payload)
 
-@app.post("/signup", response_model=UserResponse)
+@app.post("/signup", response_model=UserResponse,tags =["Register"])
 def signup(user: UserCreate, db: Session = Depends(get_db)):
     """_summary_
     Registers a new user and returns user details.
@@ -58,7 +58,7 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
     results = UserService(db)
     return results.signup(user)
     
-@app.post("/login")
+@app.post("/login",tags =["Login"])
 def login_user(user :UserLogin ,db: Session= Depends(get_db)):
     """_summary_
     Authenticates a user and returns an access token.
@@ -71,7 +71,7 @@ def login_user(user :UserLogin ,db: Session= Depends(get_db)):
     results = UserService(db)
     return results.login_user(user)
 
-@app.post("/auth/refresh")
+@app.post("/auth/refresh",tags =["Tokens"])
 def refresh(token : Refresh,db: Session= Depends(get_db)):
     """_summary_
     Refreshes the authentication token.
@@ -84,7 +84,7 @@ def refresh(token : Refresh,db: Session= Depends(get_db)):
     results= UserService(db)
     return results.refresh(token)
 
-@app.get("/")
+@app.get("/",tags =["Books"])
 def hello():
     """
     This function performs when no endpoints are given.
@@ -93,7 +93,7 @@ def hello():
     """
     return "hello"
 
-@app.get("/books/all",response_model=List[BookSchema])
+@app.get("/books/all",response_model=List[BookSchema],tags =["Books"])
 def get_books( db: Session = Depends(get_db)):
     """_summary_
     
@@ -105,7 +105,7 @@ def get_books( db: Session = Depends(get_db)):
     results= RetrieveService(db)
     return results.get_books()
 
-@app.get("/users/all",response_model=List[UserResponse])
+@app.get("/users/all",response_model=List[UserResponse],tags =["User"])
 def get_users( db: Session = Depends(get_db)):
     """_summary_
     This function displays all users.
@@ -117,7 +117,7 @@ def get_users( db: Session = Depends(get_db)):
     results= RetrieveService(db)
     return results.get_users()
 
-@app.get("/books",response_model=List[BookSchema])
+@app.get("/books",response_model=List[BookSchema],tags =["Books"])
 def paginate_books(page_number: int=Query(1,gt=0),page_size: int =Query(5,lt=10),db: Session = Depends(get_db)):
     """
     This function performs when no specific endpoints are given.
@@ -127,7 +127,7 @@ def paginate_books(page_number: int=Query(1,gt=0),page_size: int =Query(5,lt=10)
     results = RetrieveService(db)
     return results.paginate_books(page_number,page_size)
 
-@app.get("/books/{book_id}",response_model=BookSchema)
+@app.get("/books/{book_id}",response_model=BookSchema,tags =["Books"])
 def get_individual_book(book_id : int, db: Session = Depends(get_db)):
     """
     This function performs when specific endpoints are given.
@@ -139,7 +139,7 @@ def get_individual_book(book_id : int, db: Session = Depends(get_db)):
     service = RetrieveService(db) 
     return service.get_individual_book(book_id)
 
-@app.get("/books/",response_model=List[BookSchema])
+@app.get("/books/",response_model=List[BookSchema],tags =["Books"])
 def get_filtered_books(book : BookFilter = Depends(),db : Session = Depends(get_db)):
     """_summary_
     This function filters the book based on given query
@@ -152,7 +152,7 @@ def get_filtered_books(book : BookFilter = Depends(),db : Session = Depends(get_
     service = RetrieveService(db) 
     return service.get_filtered_books(book)
 
-@app.get("/authors", response_model=List[AuthorSchema])
+@app.get("/authors", response_model=List[AuthorSchema],tags =["Author"])
 def get_author(db: Session = Depends(get_db)):
     """
     This function performs when specific endpoints are given.
@@ -164,7 +164,7 @@ def get_author(db: Session = Depends(get_db)):
     service = RetrieveService(db)
     return service.get_author()
 
-@app.post("/books/{book_id}/author", response_model=AuthorSchema)
+@app.post("/books/{book_id}/author", response_model=AuthorSchema,tags =["Author"])
 def create_author(book_id : int,author_data: AuthorCreate, db: Session = Depends(get_db),user: dict = Depends(get_current_user)):
     """
     This function performs when specific endpoints are given.
@@ -176,7 +176,7 @@ def create_author(book_id : int,author_data: AuthorCreate, db: Session = Depends
     service = CreateService(db)
     return service.create_author(book_id,author_data,user)
 
-@app.post("/books",response_model=BookSchema)
+@app.post("/books",response_model=BookSchema,tags =["Books"])
 def create_book(book: BookCreate,db: Session= Depends(get_db),user: dict = Depends(get_current_user)):
     """
     This function creates the new book .
@@ -189,7 +189,7 @@ def create_book(book: BookCreate,db: Session= Depends(get_db),user: dict = Depen
     service = CreateService(db) 
     return service.create_book(book,user)
     
-@app.post("/files")
+@app.post("/files",tags =["Files"])
 def upload_file(file:UploadFile = File(),db:Session = Depends(get_db),user: dict = Depends(get_current_user)):
     """_summary_
     This function uploads file to cloudinary.
@@ -203,7 +203,7 @@ def upload_file(file:UploadFile = File(),db:Session = Depends(get_db),user: dict
     service = CreateService(db)
     return service.upload_file(file,user)
 
-@app.post("/filestream")
+@app.post("/filestream",tags =["Files"])
 def file_stream(file : UploadFile =File(),db:Session=Depends(get_db)):
     """_summary_
     This function is to stream the file
@@ -219,7 +219,7 @@ def file_stream(file : UploadFile =File(),db:Session=Depends(get_db)):
                 yield chunk
     return StreamingResponse(iterfile(), media_type="application/pdf")
 
-@app.put("/books/{book_id}",response_model=BookSchema)
+@app.put("/books/{book_id}",response_model=BookSchema,tags =["Books"])
 def update_book(book_id: int, book: BookCreate,db: Session = Depends(get_db),user: dict = Depends(get_current_user)):
     """
     This function updates the book by getting the id from user to locate the book.
@@ -232,7 +232,7 @@ def update_book(book_id: int, book: BookCreate,db: Session = Depends(get_db),use
     service = UpdateService(db) 
     return service.update_book(book_id,book,user)
 
-@app.put("/authors/{author_id}", response_model=AuthorSchema)
+@app.put("/authors/{author_id}", response_model=AuthorSchema,tags =["Author"])
 def update_author(author_id: int, author: AuthorCreate, db: Session = Depends(get_db),user: dict = Depends(get_current_user)):
     """
     This function updates the author by getting the id from user to locate the author.
@@ -245,7 +245,7 @@ def update_author(author_id: int, author: AuthorCreate, db: Session = Depends(ge
     service = UpdateService(db)
     return service.update_author(author_id, author,user)
 
-@app.put("/users/{user_id}", response_model= UserResponse)
+@app.put("/users/{user_id}", response_model= UserResponse,tags =["User"])
 def update_user(user_id : int, users : UserCreate, db: Session = Depends(get_db),user : dict= Depends(get_current_user)):
     """
     This function updates the  user credential.
@@ -257,7 +257,7 @@ def update_user(user_id : int, users : UserCreate, db: Session = Depends(get_db)
     service = UpdateService(db)
     return service.update_user(user_id,users,user)
 
-@app.delete("/books/{book_id}")
+@app.delete("/books/{book_id}",tags =["Books"])
 def delete_books(book_id : int,db: Session = Depends(get_db),user: dict = Depends(get_current_user)):
     """
     This function which gets the id of he book which needs to be deleted.
@@ -267,7 +267,7 @@ def delete_books(book_id : int,db: Session = Depends(get_db),user: dict = Depend
     service = DeleteService(db) 
     return service.delete_book(book_id,user)
 
-@app.delete("/authors/{author_id}")
+@app.delete("/authors/{author_id}",tags =["Author"])
 def delete_author(author_id : int, db: Session = Depends(get_db),user: dict = Depends(get_current_user)):
     """
     This function which gets the id of the delete which needs to be deleted.
